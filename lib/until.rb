81c39b11c6659b9fcd32eee390cd4626cb953f04
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-22 14:20:49 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
-levitation_force = 6
+
 def using_until
-  puts "Wingardium Leviosa"
+  levitation_force = 6
   #your code here
-  levitation_force+=1
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force+=1
+    
+  end
+    
 end
+
